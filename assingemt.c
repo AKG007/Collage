4c948ef40ffd51450af007b56c6a3405b9055a86
aniruddhagawali <aniruddhagawali05@gmail.com>
2022-03-23 06:54:20 +0530

    added more asssignment

--- assingemt.c
+++ assingemt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<conio.h>
 
 int sum_of_selected_no(int arr[3][3]) // 1 to sum up the selected members of the 2 Dimensional Array (TDA)
 {
@@ -75,9 +76,8 @@ void print_2darr(int arr[3][3])
 void main()
 
 {
-    /*
 
-    int tda[3][3] = {10, 11, 12, 13, 14, 15, 16, 17, 18};
+    int tda[3][3] = {10, 11, 12, 13, 14, 15, 16, 17, 18},r,c;
     // 1 to sum up the selected members of the 2 Dimensional Array (TDA)
     printf("Made an array 3x3");
     print_2darr(tda);
@@ -92,9 +92,9 @@ void main()
     int br = 0, bc = 0, sr = 0, sc = 0, big = tda[0][0], small = tda[0][0];
     printf("Orignal 2d array\n");
     print_2darr(tda);
-    for (int r = 0; r < 3; r++)
+    for (r = 0; r < 3; r++)
     {
-        for (int c = 0; c < 3; c++)
+        for (c = 0; c < 3; c++)
         {
             if (tda[r][c] > big)
             {
@@ -120,7 +120,7 @@ void main()
     // -------------------------------------------------------------------------------------
 
     //  4 given an array to print the sum of digits of each number in the array
-    int sum = 0, arr[5] = {10, 11, 12, 13, 14}, num, i, r;
+    int sum = 0, arr[5] = {10, 11, 12, 13, 14}, num,i;
     printf("Q4 Given an array to print the sum of digits of each number in the array\n\n");
     for (r = 0; r < 5; r++)
     {
@@ -147,27 +147,34 @@ void main()
     // --------------------------------------------------------------------------------------
 
     // 5 check the armstrong numbers, mirror numbers in the array
-    int arr[3] = {121, 234, 153}, r, check_amstrong_no, check_mirror_no;
-    printf("Q5 Check the armstrong numbers, mirror numbers in the array\n\n");
+    int check_amstrong_no, check_mirror_no;
+    int arr_[3] = {121, 234, 153};
+    printf("Q5 Check the armstrong numbers, mirror numbers in the array\n\nArray");
+    for ( r = 0; r < 3; r++)
+    {
+        printf(" %3d ",arr_[r]);
+    }
+    printf("\n");
+    
     for (r = 0; r < 3; r++)
     {
-        check_amstrong_no = check_amstrong(arr[r]);
-        check_mirror_no = check_mirror(arr[r]);
+        check_amstrong_no = check_amstrong(arr_[r]);
+        check_mirror_no = check_mirror(arr_[r]);
         if (check_amstrong_no == 1)
         {
-            printf("\n%d is an amstrong no", arr[r]);
+            printf("\n%d is an amstrong no", arr_[r]);
         }
         else
         {
-            printf("\n%d is not an amstrong no", arr[r]);
+            printf("\n%d is not an amstrong no", arr_[r]);
         }
         if (check_mirror_no == 1)
         {
-            printf("\n%d is an mirror no", arr[r]);
+            printf("\n%d is an mirror no", arr_[r]);
         }
         else
         {
-            printf("\n%d is not an mirror no", arr[r]);
+            printf("\n%d is not an mirror no", arr_[r]);
         }
         printf("\n");
     }
@@ -177,8 +184,7 @@ void main()
 
     // ---------------------------------------------------------------------------------------------
     // 7 to rotate the elements of the array to right or to left!
-    printf("Rotate the elements of the array to right or to left!\nSo Rotating array to left\n\nOrignal");
-    // arr[5]=[10,11,12,13,14];
+    printf("\nQ7 Rotate the elements of the array to right or to left!\nSo Rotating array to left\n\nOrignal");
     print_2darr(tda);
     printf("After Rotate\n\n");
     for (int r = 2; r >= 0; r--)
@@ -189,9 +195,10 @@ void main()
         }
         printf("\n");
     }
+    getch();
     // -----------------------------------------------------------------------------------------------------
 
-    printf("8. swap 2 specified  rows of the tda\nOrginal");
+    printf("\nQ8. swap 2 specified  rows of the tda\nOrginal");
     print_2darr(tda);
     printf("After Swap\n");
     for (int r = 0; r < 3; r++)
@@ -200,13 +207,14 @@ void main()
         {
             printf(" %d ", tda[2 - r][c]);
         }
+        printf("\n");
     }
+    getch();
 
     // ---------------------------------------------------------------------------------------------
 
     //  11 program to print the row number of the biggest elem in each row
-    int big;
-    printf("11Biggest no in each row");
+    printf("\n\nQ11 Biggest no in each row\n\n");
     for (int r = 0; r < 3; r++)
     {
         big = tda[r][0];
@@ -219,47 +227,51 @@ void main()
         }
         printf("\nIn first row %2d", big);
     }
+    getch();
 
     // -----------------------------------------------------------------------------------------------------
 
     // 13 given a tda (2 dimensional Array), wap to print the rowsums, colsums separately and to give the grand sum
-    int tda[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, r, c, rsum = 0, csum = 0;
-    printf("Section B\n\n");
+    int rsum = 0, csum = 0, tda_[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("\n\nSection B\n\n");
     printf("Q13 given a tda (2 dimensional Array), wap to print the rowsums, colsums separately and to give the grand sum\n\n");
     for (int r = 0; r < 3; r++)
     {
         for (c = 0; c < 3; c++)
         {
-            printf(" %d ", tda[r][c]);
-            rsum = rsum + tda[r][c];
-            csum = csum + tda[c][r];
+            printf(" %d ", tda_[r][c]);
+            rsum = rsum + tda_[r][c];
+            csum = csum + tda_[c][r];
         }
         printf("\trow sums is %d", rsum);
         printf("\tcols sums is %d", csum);
         printf("\n");
     }
-    printf("\"Press enter to go next question\"");
+    printf("\n\"Press enter to go next question\"");
     getch();
 
-    */
+    // ---------------------------------------------------------------------------------------
 
     //    15 to print the following shapes off/from a 9x9square matrix. Plus sign, tick mark
     // 	 hockey stick, spoon, inverted U, inverted C of desirable proportions
-    int tda[9][9], num = 1, r, c;
-    for (r = 0; r < 9; r++)
+    int _tda[9][9];
+    num = 1;
+    for (r = 0; r < 9; r++) // adding numbers to the array by help of loop
     {
         for (c = 0; c < 9; c++)
         {
-            tda[r][c] = num;
+            _tda[r][c] = num;
             num++;
         }
     }
 
-    for (r = 0; r < 9; r++)
+    printf("\n\n\" 15 to print the following shapes off/from a 9x9square matrix.\nPlus sign, tick mark hockey stick, spoon, inverted U, inverted C of desirable proportions\"\nArray of 9x9\n\n");
+
+    for (r = 0; r < 9; r++);
     {
         for (c = 0; c < 9; c++)
         {
-            printf(" %2d ", tda[r][c]);
+            printf(" %2d ", _tda[r][c]);
         }
         printf("\n");
     }
@@ -272,30 +284,30 @@ void main()
         {
             if (r == 4 || c == 4)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
     // Tick mark
     printf("\n\nTick mark\n\n");
     for (r = 0; r < 9; r++)
     {
         for (c = 0; c < 9; c++)
         {
-            if (c ==0  && r >4 || 8-r ==c)
+            if (c == 0 && r > 4 || 8 - r == c)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
 
     // Hockey stick
     printf("\n\nHockey stick\n\n");
@@ -303,16 +315,16 @@ void main()
     {
         for (c = 0; c < 9; c++)
         {
-            if (c >=0&& c<=1  && r >5 ||r==8 && c>=0 && c<2|| 7-r ==c)
+            if (c >= 0 && c <= 1 && r > 5 || r == 8 && c >= 0 && c < 2 || 7 - r == c)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
 
     // Spoon
     printf("\n\nSpoon\n\n");
@@ -320,16 +332,16 @@ void main()
     {
         for (c = 0; c < 9; c++)
         {
-            if (c ==0  && r >4 ||r==8 && c>=0 && c<4||c ==3  && r >4|| c>=3 && 7-r ==c)
+            if (c == 0 && r > 4 || r == 8 && c >= 0 && c < 4 || c == 3 && r > 4 || c >= 3 && 7 - r == c)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
 
     // inverted U
     printf("\n\ninverted U\n\n");
@@ -337,30 +349,32 @@ void main()
     {
         for (c = 0; c < 9; c++)
         {
-            if (c==0||r==0||c==8)
+            if (c == 0 || r == 0 || c == 8)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
+
     // inverted C
     printf("\n\ninverted C\n\n");
     for (r = 0; r < 9; r++)
     {
         for (c = 0; c < 9; c++)
         {
-            if (r==0||c==8||r==8)
+            if (r == 0 || c == 8 || r == 8)
             {
-                printf(" %2d ", tda[r][c]);
+                printf(" %2d ", _tda[r][c]);
             }
             else
                 printf("  . ");
-            
         }
         printf("\n");
     }
+    getch();
+    // --------------------------------------------------------------------------------------------------------
 }
